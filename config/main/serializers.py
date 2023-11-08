9a3bb6bb50bdd611c7a74528267fb956d7764fc6
Gerasimov <geraasim97@mail.ru>
2023-11-08 14:09:30 +0300

    2

--- config/main/serializers.py
+++ config/main/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 
-from main.models import Course, Lesson , Payment
-
+from main.models import Course, Lesson , Payment, Subscription
+from main.validators import LinkValidator
 
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Курс"""
     lesson_count = serializers.SerializerMethodField(read_only=True)
     lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'lesson_count', 'lessons']
+        fields = ['id', 'title', 'description', 'lesson_count', 'lessons', 'is_subscribed']
 
         def get_lesson_count(self, instance):
             return instance.lesson_set.all().count()
+
+        def get_is_subscribed(self, instance):
+            if Subscription.objects.filter(course=instance).count() == 0:
+                return False
+            else:
+                return True
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -21,6 +28,7 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [LinkValidator(field='link')]
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -28,4 +36,11 @@ class PaymentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Payment
+        fields = '__all__'
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Подписка"""
+
+    class Meta:
+        model = Subscription
         fields = '__all__'
--- config/main/views.py
+++ config/main/views.py
@@ -1,31 +1,37 @@
-from rest_framework import viewsets, generics
-from rest_framework.generics import CreateAPIView, DestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
-from main.models import Course, Lesson
-from main.serializers import CourseSerializer, LessonSerializer
+from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
-from main.models import Course, Lesson, Payment
-from main.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
-from main.permissions import CoursePermissions, IsModerator, IsOwner
+from rest_framework.generics import CreateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+
+from main.models import Course, Lesson, Payment, Subscription
+from main.paginators import PagePagination
+from main.permissions import CoursePermissions, IsModerator, IsOwner
+from main.serializers import CourseSerializer, LessonSerializer, PaymentSerializer, SubscriptionSerializer
 from rest_framework.response import Response
 class CourseViewSet(viewsets.ModelViewSet):
     """ViewSet для курса"""
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    pagination_class = PagePagination
     permission_classes = [IsAuthenticated, CoursePermissions]
 
     def perform_create(self, serializer):
         new_course = serializer.save()
         new_course.created_by = self.request.user
         new_course.save()
-
     def list(self, request):
         if self.request.user.is_moderator:
             queryset = Course.objects.all()
         else:
             queryset = Course.objects.filter(created_by=self.request.user)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = CourseSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
         serializer = CourseSerializer(queryset, many=True)
+
         return Response(serializer.data)
 
 
@@ -33,17 +39,15 @@ class LessonCreateAPIView(CreateAPIView):
     """Контроллер создания урока"""
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated, ~IsModerator]
-
     def perform_create(self, serializer):
         new_lesson = serializer.save()
         new_lesson.created_by = self.request.user
         new_lesson.save()
-
-
 class LessonListAPIView(generics.ListAPIView):
     """Контроллер просмотра списка уроков"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    pagination_class = PagePagination
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
@@ -52,28 +56,21 @@ class LessonListAPIView(generics.ListAPIView):
         else:
             queryset = Lesson.objects.filter(created_by=self.request.user)
         return queryset
-
-
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     """Контроллер просмотра конкретного урока"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, IsOwner | IsModerator]
-
-
 class LessonUpdateAPIView(generics.UpdateAPIView):
     """Контроллер редактирования урока"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, IsOwner | IsModerator]
-
-
 class LessonDestroyAPIView(DestroyAPIView):
     """Контроллер удаления урока"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, IsOwner]
-
 class PaymentListAPIView(generics.ListAPIView):
     """Контроллер просмотра списка платежей"""
     serializer_class = PaymentSerializer
@@ -81,4 +78,13 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = [OrderingFilter, DjangoFilterBackend]
     ordering_fields = ['date']
     filterset_fields = ['course', 'lesson', 'method']
+    permission_classes = [IsAuthenticated]
+class SubscriptionCreateAPIView(CreateAPIView):
+    """Контроллер установки подписки пользователя"""
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+class SubscriptionDestroyAPIView(DestroyAPIView):
+    """Контроллер удаления подписки у пользователя"""
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
     permission_classes = [IsAuthenticated]
